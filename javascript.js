e918ce1dddaf46b8c43de6e929a05456f34ddc79
Christian Lamothe <christianlamothe@Christians-Air.home>
2017-05-08 23:30:51 -0400

    added 2 more quotes

--- javascript.js
+++ javascript.js
@@ -8,7 +8,10 @@ var quotes= [
     "Write it on your heart that every day is the best day in the year. Ralph Waldo Emerson",
     "Health is the greatest gift, contentment the greatest wealth, faithfulness the best relationship. Buddha",
     "The best way to find yourself is to lose yourself in the service of others. Mahatma Gandhi",
-    "I believe in pink. I believe that laughing is the best calorie burner. I believe in kissing, kissing a lot. I believe in being strong when everything seems to be going wrong. I believe that happy girls are the prettiest girls. I believe that tomorrow is another day and I believe in miracles. Audrey Hepburn"
+    "I believe in pink. I believe that laughing is the best calorie burner. I believe in kissing, kissing a lot. I believe in being strong when everything seems to be going wrong. I believe that happy girls are the prettiest girls. I believe that tomorrow is another day and I believe in miracles. Audrey Hepburn",
+    "Every man's life ends the same way. It is only the details of how he lived and how he died that distinguish one man from another. Ernest Hemingway",
+    "It is better to be alone than in bad company. George Washington"
+
 ]
 
 function newQuote() {
